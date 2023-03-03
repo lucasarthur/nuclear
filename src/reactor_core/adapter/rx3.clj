b3d925a33aaa264f10967f5a4453a5408df20a00
Lucas Arthur <luca.oetdbem@gmail.com>
2023-03-03 13:20:25 -0300

    add rx3 adapter

---
+++ src/reactor_core/adapter/rx3.clj
@@ -0,0 +1,44 @@
+(ns reactor-core.adapter.rx3
+  (:require
+   [reactor-core.util.utils :as u]
+   [reactor-core.util.sam :as sam])
+  (:import
+   (reactor.adapter.rxjava RxJava3Adapter)
+   (io.reactivex BackpressureStrategy)))
+
+(defn flowable->flux [flowable]
+  (RxJava3Adapter/flowableToFlux flowable))
+
+(defn flux->flowable [flux]
+  (RxJava3Adapter/fluxToFlowable flux))
+
+(defn mono->flowable [mono]
+  (RxJava3Adapter/monoToFlowable mono))
+
+(defn mono->completable
+  ([mono] (RxJava3Adapter/monoToCompletable mono))
+  ([discard-handler mono] (RxJava3Adapter/monoToCompletable mono (sam/->consumer discard-handler))))
+
+(defn completable->mono [completable]
+  (RxJava3Adapter/completableToMono completable))
+
+(defn mono->single [mono]
+  (RxJava3Adapter/monoToSingle mono))
+
+(defn single->mono [single]
+  (RxJava3Adapter/singleToMono single))
+
+(defn observable->flux
+  ([observable] (observable->flux :buffer observable))
+  ([strategy observable] (RxJava3Adapter/observableToFlux
+                          observable
+                          (u/keyword->enum BackpressureStrategy strategy))))
+
+(defn flux->observable [flux]
+  (RxJava3Adapter/fluxToObservable flux))
+
+(defn maybe->mono [maybe]
+  (RxJava3Adapter/maybeToMono maybe))
+
+(defn mono->maybe [mono]
+  (RxJava3Adapter/monoToMaybe mono))

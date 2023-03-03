bb7c0b2abb9f7b4905a5d2b8883a1b2444ad98ea
Lucas Arthur <luca.oetdbem@gmail.com>
2023-03-03 12:08:06 -0300

    add reactor test deps

--- project.clj
+++ project.clj
@@ -4,5 +4,6 @@
   :license {:name "GNU General Public License v3.0"
             :url "https://www.gnu.org/licenses/gpl-3.0.pt-br.html"}
   :dependencies [[org.clojure/clojure "1.11.1"]
-                 [io.projectreactor/reactor-core "3.5.3"]]
+                 [io.projectreactor/reactor-core "3.5.3"]
+                 [io.projectreactor/reactor-test "3.5.3"]]
   :target-path "target/%s")

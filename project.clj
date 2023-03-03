7b78774bbdf3ef70e81da3eab4ca724a048c39b3
Lucas Arthur <luca.oetdbem@gmail.com>
2023-03-03 12:14:57 -0300

    add reactor adapter deps

--- project.clj
+++ project.clj
@@ -5,5 +5,6 @@
             :url "https://www.gnu.org/licenses/gpl-3.0.pt-br.html"}
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [io.projectreactor/reactor-core "3.5.3"]
-                 [io.projectreactor/reactor-test "3.5.3"]]
+                 [io.projectreactor/reactor-test "3.5.3"]
+                 [io.projectreactor.addons/reactor-adapter "3.5.0"]]
   :target-path "target/%s")

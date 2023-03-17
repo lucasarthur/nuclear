95536390a69d7a8a082b93022c0370b5a9168a28
Lucas Arthur <luca.oetdbem@gmail.com>
2023-03-17 19:48:33 -0300

    v0.1.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject nuke "0.1.0"
+(defproject nuke "0.1.1"
   :description "A Clojure wrapper for Project Reactor"
   :url "https://github.com/lucasarthur/nuke"
   :license {:name "GNU General Public License v3.0"

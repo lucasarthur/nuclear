8256aee29fce98472cd2541311dba163b4ec16f5
Lucas Arthur <luca.oetdbem@gmail.com>
2023-03-20 00:29:34 -0300

    rename project to nuclear/v0.1.2

--- src/nuclear/operations.clj
+++ src/nuclear/operations.clj
@@ -1,27 +1,27 @@
 ;; Copyright (c) 2023 Lucas Arthur
 ;;
-;; This file is part of Nuke, an open-source library whose goal is to
+;; This file is part of Nuclear, an open-source library whose goal is to
 ;; use Project Reactor with Clojure in an idiomatic and simplified way.
 ;;
-;; Nuke is free software: you can redistribute it and/or modify
+;; Nuclear is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation, either version 3 of the License, or
 ;; (at your option) any later version.
 ;;
-;; Nuke is distributed in the hope that it will be useful,
+;; Nuclear is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 ;; GNU General Public License for more details.
 ;;
 ;; You should have received a copy of the GNU General Public License
-;; along with Nuke. If not, see <http://www.gnu.org/licenses/>.
+;; along with Nuclear. If not, see <http://www.gnu.org/licenses/>.
 
-(ns nuke.operations
+(ns nuclear.operations
   (:refer-clojure :exclude [reduce distinct next count repeat merge-with filter map mapcat take take-last take-while])
   (:require
-   [nuke.flux]
-   [nuke.mono]
-   [nuke.protocols :as p]))
+   [nuclear.flux]
+   [nuclear.mono]
+   [nuclear.protocols :as p]))
 
 (defn filter [predicate publisher]
   (p/-filter publisher predicate))
